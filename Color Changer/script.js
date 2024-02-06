7f79234239c7ffdb19c9c4326748d4d1fe8103df
saurav kumar <kumar.saurav5421@gmail.com>
2024-02-06 16:27:57 +0530

    final

---
+++ Color Changer/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.button');
+// console.log(buttons);
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e)=>{
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id==='red'){
+            body.style.backgroundColor = e.target.id;
+        } else if(e.target.id==='green'){
+            body.style.backgroundColor = e.target.id;
+        } else if(e.target.id==='blue'){
+            body.style.backgroundColor = e.target.id;
+        } else if(e.target.id==='yellow'){
+            body.style.backgroundColor = e.target.id;
+        } else {
+            body.style.backgroundColor = 'white';
+        }
+    })
+} )
